b2528cc182a2db2b1d741f6de2e9032cbd793acd
Javier Rojas <jrojas03.08.98@gmail.com>
2019-10-06 01:57:16 -0400

    mock CRUD created.

---
+++ src/main/java/Classes/Controller.java
@@ -0,0 +1,29 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class Controller {
+    private static Controller instance;
+    private ArrayList<Estudiante> estudiantes;
+
+    private Controller(){
+        super();
+        this.estudiantes = new ArrayList<>();
+    }
+
+    public static Controller getInstance() {
+        if(instance == null) {
+            instance = new Controller();
+        }
+
+        return instance;
+    }
+
+    public ArrayList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(ArrayList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,55 @@
+import Classes.Controller;
+import Classes.Estudiante;
+
+import java.util.ArrayList;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+
         get("/", (request,response) -> {
-            return "HAI, u started me program :D";
+            ArrayList<Estudiante> estudiantes = Controller.getInstance().getEstudiantes();
+            ArrayList<String> nombres = new ArrayList<>();
+            for (Estudiante estudiante: estudiantes) {
+                nombres.add(estudiante.getNombre());
+            }
+            return nombres;
+        });
+
+        get("/add", (request, response) -> {
+            ArrayList<Estudiante> estudiantes = Controller.getInstance().getEstudiantes();
+            Estudiante estudiante = new Estudiante(2020,"Jose","martinez","809-987-1111");
+            estudiantes.add(estudiante);
+            Estudiante estudiante2 = new Estudiante(2021,"Daniel","martinez","809-987-1111");
+            estudiantes.add(estudiante2);
+            Controller.getInstance().setEstudiantes(estudiantes);
+            response.redirect("/");
+            return "";
+        });
+
+        get("/delete", (request, response) -> {
+            ArrayList<Estudiante> estudiantes = Controller.getInstance().getEstudiantes();
+            for (Estudiante estudiante: estudiantes) {
+                if (estudiante.getMatricula() == 2020 ) {
+                    estudiantes.remove(estudiante);
+                }
+            }
+            Controller.getInstance().setEstudiantes(estudiantes);
+            response.redirect("/");
+            return "";
+        });
+
+        get("/update", (request, response) -> {
+            ArrayList<Estudiante> estudiantes = Controller.getInstance().getEstudiantes();
+            for(Estudiante estudiante: estudiantes) {
+                if (estudiante.getMatricula() == 2020 ){
+                    estudiante.setNombre("Enzo");
+                }
+            }
+            Controller.getInstance().setEstudiantes(estudiantes);
+            response.redirect("/");
+            return "";
         });
     }
 }
